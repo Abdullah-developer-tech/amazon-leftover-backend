c43d9b757b99083cf190efda157da5c661d836a9
Abdullah-developer-tech <aslamabdullah288@gmail.com>
2026-09-04 01:15:04 +0500

    direct mongodb url implementation fix

--- server.js
+++ server.js
@@ -28,15 +28,11 @@ app.get('/', (req, res) => res.send('Ecommerce API is running'));
 
 const PORT = process.env.PORT || 5000;
 
-// Fallback check taake agar MONGO_URI na mile toh local connection ki bajaye error saaf nazar aaye
-const MONGO_URL = process.env.MONGO_URL;
-
-if (!MONGO_URI) {
-  console.error('CRITICAL ERROR: MONGO_URI environment variable is not defined!');
-}
+// Direct MongoDB connection string taake Render par koi error na aaye
+const MONGO_URL = "mongodb+srv://aslamabdullah288_db_user:Abd12345@abdullah.2zmjnx6.mongodb.net/my-custom-app?appName=Abdullah";
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URL)
   .then(() => {
     console.log('MongoDB connected successfully');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
