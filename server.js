b352cfa6af2fffd76c356e5549cf66fa51962849
Abdullah-developer-tech <aslamabdullah288@gmail.com>
2026-09-04 10:22:05 +0500

    add database name to connection string

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MongoDB Direct Connection
-mongoose.connect("mongodb+srv://aslamabdullah288_db_user:Abd12345@abdullah.2zmjnx6.mongodb.net/?retryWrites=true&w=majority&appName=Abdullah")
+// MongoDB Direct Connection (Database name 'ecommerce' added)
+mongoose.connect("mongodb+srv://aslamabdullah288_db_user:Abd12345@abdullah.2zmjnx6.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Abdullah")
   .then(() => console.log("MongoDB Connected Successfully!"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
